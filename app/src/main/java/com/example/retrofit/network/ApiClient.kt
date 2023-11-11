4d436f2e566654aa132fb8d2687ee2199106adcf
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-11-11 19:22:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/retrofit/network/ApiClient.kt
@@ -0,0 +1,26 @@
+package com.example.retrofit.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    val url = "https://dummy.restapiexample.com/api/v1/"
+
+    fun getInstance(): ApiService {
+        val mHttpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        val mOkHttpClient = OkHttpClient
+            .Builder()
+            .addInterceptor(mHttpLoggingInterceptor)
+            .build()
+        val builder = Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(mOkHttpClient)
+            .build()
+        return builder.create(ApiService::class.java)
+    }
+}
